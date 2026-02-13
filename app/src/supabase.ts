ecea1456fecd8bb0a05b7a53d414425e9d26dbcb
harshavarthan <harshavarthankarunakaran@gmail.com>
2026-02-13 11:19:23 +0530

    fixed the 401 error

--- app/src/supabase.ts
+++ app/src/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zplrfkkyaxalniiofdsp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwbHJma2t5YXhhbG5paW9mZHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4NzQ2MDIsImV4cCI6MjA4NjQ1MDYwMn0._6Qf5J4ZyBbSarryZwT6jeEDrWWcmF4vPYGIoFVEyaw'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
